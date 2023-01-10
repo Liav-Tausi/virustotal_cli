9285628c14fd7a80dc34e2d45e9eba1d89d3574f
liav tausi <liavt242@gmail.com>
2023-01-10 16:05:47 +0200

    main

--- virustotal_automator/vt_main.py
+++ virustotal_automator/vt_main.py
@@ -1,35 +1,21 @@
-from vt_api_key_for_test import *
-from vt_file import *
-from vt_url import *
-
-
+import os
+import pickle
+import vt_terminal
 
 
 if __name__ == '__main__':
-    url = VTUrl(url=r'https://www.ynet.co.il/', vt_key=vt_key)
-    print(url.post_get_url())
+    try:
+        if not os.path.exists('vt_auto.pickle'):
+            vt_automator = vt_terminal.main()
+        else:
+            with open('vt_auto.pickle', 'rb') as fh:
+                vt_automator = pickle.load(fh)
 
+        print(vt_terminal.main())
 
-    url = VTUrl(url=r'https://www.ynet.co.il/', vt_key=vt_key)
-    print(url.post_get_url())
+    except Exception():
+        print("Error restart program.")
 
-
-    url = VTUrl(url=r'https://edulabs.co.il/', vt_key=vt_key)
-    print(url.post_get_url())
-
-
-    url = VTUrl(url=r'https://edulabs.co.il/', vt_key=vt_key)
-    print(url.post_get_url())
-
-
-    url = VTUrl(url=r'https://mixedanalytics.com/', vt_key=vt_key)
-    print(url.post_get_url())
-
-    file = VTFile(file=r'C:\Users\liavt\PycharmProjects\LernningPython\edulabs\file_handler\f_h_files\csv_ex.csv',
-                  vt_key=vt_key)
-    print(file.post_get_file())
-
-
-    file = VTFile(file=r'C:\Users\liavt\PycharmProjects\LernningPython\edulabs\file_handler\f_h_files\json_ex.json',
-                  vt_key=vt_key)
-    print(file.post_get_file())
+    finally:
+        with open('vt_auto.pickle', 'wb') as fh:
+            pickle.dump(vt_automator, fh)
