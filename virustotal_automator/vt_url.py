b8fce29364be0c1fc5aca9559b08a38d729669c1
liav tausi <liavt242@gmail.com>
2023-01-12 14:21:24 +0200

    url analysis

--- virustotal_automator/vt_url.py
+++ virustotal_automator/vt_url.py
@@ -21,7 +21,7 @@ class VTUrl(VTAutomator):
         for every_url in url:
             if not isinstance(every_url, str):
                 raise vt_exeptions.UrlError()
-        self.__url: tuple[str, ...] = url
+            self.__url: tuple[str, ...] = url
 
         if not isinstance(vt_key, str) or not vt_key:
             raise vt_exeptions.ApiKeyError()
@@ -39,51 +39,49 @@ class VTUrl(VTAutomator):
     def vt_key(self) -> str:
         return self.__vt_key
 
-    def _get_req_url(self) -> dict[str, dict]:
+    def _get_req_url(self, _url) -> dict[str, dict]:
         if self.requests_amount_limit_counter < 500 and \
                 self.requests_per_minute_limit_counter < 4:
 
             self.set_amount_limit_counter()
             self.set_per_minute_limit_counter()
 
-            for index, every_url in enumerate(self.url):
-                headers: dict = {
-                    "accept": "application/json",
-                    "x-apikey": self.vt_key
-                }
-                url_id: str = base64.urlsafe_b64encode(f'{self.url[index]}'.encode()).decode().strip('=')
-                req: 'requests' = requests.get(url=self.get_vt_api_url + url_id, headers=headers)
+            headers: dict = {
+                "accept": "application/json",
+                "x-apikey": self.vt_key
+            }
+            url_id: str = base64.urlsafe_b64encode(f'{_url}'.encode()).decode().strip('=')
+            req: 'requests' = requests.get(url=self.get_vt_api_url + url_id, headers=headers)
 
-                if req.status_code >= 400:
-                    raise vt_exeptions.RequestFailed()
-                elif bool(req.json()):
-                    return req.json()
-                else:
-                    raise vt_exeptions.EmptyContentError()
+            if req.status_code >= 400:
+                raise vt_exeptions.RequestFailed()
+            elif bool(req.json()):
+                return req.json()
+            else:
+                raise vt_exeptions.EmptyContentError()
 
-    def _post_req_url(self) -> dict[str, dict]:
+    def _post_req_url(self, _url) -> dict[str, dict]:
         if self.requests_amount_limit_counter < 500 and \
                 self.requests_per_minute_limit_counter < 4:
 
             self.set_amount_limit_counter()
             self.set_per_minute_limit_counter()
 
-            for index, every_url in enumerate(self.url):
-                payload: str = f"url={self.url[index]}"
+            payload: str = f"url={_url}"
 
-                headers: dict = {
-                    "accept": "application/json",
-                    "x-apikey": self.vt_key,
-                    "content-type": "application/x-www-form-urlencoded"
-                }
-                req: 'requests' = requests.post(self.post_vt_api_url, data=payload, headers=headers)
+            headers: dict = {
+                "accept": "application/json",
+                "x-apikey": self.vt_key,
+                "content-type": "application/x-www-form-urlencoded"
+            }
+            req: 'requests' = requests.post(self.post_vt_api_url, data=payload, headers=headers)
 
-                if req.status_code >= 400:
-                    raise vt_exeptions.RequestFailed()
-                if bool(req.json()):
-                    return req.json()
-                else:
-                    raise vt_exeptions.EmptyContentError()
+            if req.status_code >= 400:
+                raise vt_exeptions.RequestFailed()
+            if bool(req.json()):
+                return req.json()
+            else:
+                raise vt_exeptions.EmptyContentError()
 
     @VTAutomator.get_cache_url
     def get_url(self):
@@ -93,8 +91,8 @@ class VTUrl(VTAutomator):
         else:
             raise FileNotFoundError()
 
-    def post_url(self) -> str:
-        rep: str = self._post_req_url().get('data')['type']
+    def post_url(self, _url: str = None) -> str:
+        rep: str = self._post_req_url(_url).get('data')['type']
         if rep is not None:
             return rep
         else:
@@ -105,13 +103,13 @@ class VTUrl(VTAutomator):
             _url = self.url
         for url_inx in self.url:
             if url_inx not in self.cache_url_dict:
-                self.post_url()
-        for _ in range(10):
-            res_code = self.get_url()
-            if isinstance(res_code, int):
-                return _url, res_code
-            else:
-                time.sleep(30)
+                self.post_url(_url)
+            for _ in range(1):
+                res_code = self.get_url(_url)
+                if isinstance(res_code, int):
+                    return _url, res_code
+                else:
+                    time.sleep(20)
 
     def post_get_urls(self) -> list[tuple]:
         results: list = list()
@@ -121,10 +119,10 @@ class VTUrl(VTAutomator):
                 results.append(f.result())
         return results
 
-    def _get_req_file(self):
+    def _get_req_file(self, _file):
         pass
 
-    def _post_req_file(self):
+    def _post_req_file(self, _file):
         pass
 
 
