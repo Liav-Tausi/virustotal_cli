60992283f79ba7f090e3eee3852c2ffa4524c2c2
liav tausi <liavt242@gmail.com>
2023-01-12 21:08:37 +0200

    main

--- virustotal_automator/vt_main.py
+++ virustotal_automator/vt_main.py
@@ -2,10 +2,8 @@ import time
 import vt_terminal
 
 if __name__ == '__main__':
-    start = time.perf_counter()
     print(vt_terminal.main())
-    end = time.perf_counter()
-    print(f"{round(end - start, 2)} second(s)")
+
 
 
 
