5f7e508741c873642c3dce28460d4d032ff7126e
liav tausi <liavt242@gmail.com>
2023-01-12 14:21:56 +0200

    main

--- virustotal_automator/vt_main.py
+++ virustotal_automator/vt_main.py
@@ -1,18 +1,14 @@
-import os
-import pickle
+import time
 import vt_terminal
 
 
 if __name__ == '__main__':
-    try:
-        if not os.path.exists('vt_auto.pickle'):
-            vt_automator = vt_terminal.main()
-        else:
-            with open('vt_auto.pickle', 'rb') as fh:
-                vt_automator = pickle.load(fh)
+    start = time.perf_counter()
+    print(vt_terminal.main())
+    end = time.perf_counter()
+    print(f"{round(end - start, 2)} second(s)")
 
-        print(vt_terminal.main())
 
-    finally:
-        with open('vt_auto.pickle', 'wb') as fh:
-            pickle.dump(vt_automator, fh)
+
+
+
