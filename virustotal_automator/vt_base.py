5139d28169fca44d31dc22a228d1423f582ff78c
liav tausi <liavt242@gmail.com>
2023-01-13 19:01:20 +0200

    abstract parent/base

--- virustotal_automator/vt_base.py
+++ virustotal_automator/vt_base.py
@@ -4,23 +4,27 @@ created by: liav tausi
 date: 1/12/2023
 """
 
-
-
 from datetime import timedelta, date, datetime
 from abc import ABC, abstractmethod
 from threading import Lock
+import vt_exeptions
 import functools
+import requests
 import atexit
 import pytz
-import time
 import json
 import os
+
+
 
 
 class VTAutomator(ABC):
     """
     store the URLs of the VirusTotal APIs GET and POST endpoints for URLs and files.
     """
+    # _____stats____ #
+    __USER_QUOTA_SUMMARY: str = 'https://www.virustotal.com/api/v3/users/'
+
     # _____urls_____ #
     __GET_VT_API_URL: str = r'https://www.virustotal.com/api/v3/urls/'
     __POST_VT_API_URL: str = r'https://www.virustotal.com/api/v3/urls'
@@ -36,16 +40,21 @@ class VTAutomator(ABC):
         it tries to open and read the data from two json files 'vt_cache_url.json' and 'vt_cache_file.json'
         and loads the data in the dicts __cache_url_dict and __cache_file_dict.
         It also sets the requests amount and requests per minute limits for interacting with the VirusTotal API.
-        :param ref_cache_month: cache refresh rate
+        param ref_cache_month: cache refresh rate
         """
         # registering a save_data function to be called when the program exits
         atexit.register(self.save_data)
+        self.__api_key = self.api_key
+        self._get_allaowens()
 
-        self.__requests_amount_limit: int = 500
-        self.__requests_amount_limit_counter: int = 0
+        self.__requests_monthly_amount_limit = self.requests_monthly_amount_limit
+        self.__requests_monthly_amount_limit_counter = self.requests_monthly_amount_limit_counter
 
-        self.__requests_per_minute_limit: int = 4
-        self.__requests_per_minute_limit_counter = 0
+        self.__requests_daily_amount_limit = self.requests_daily_amount_limit
+        self.__requests_daily_amount_limit_counter = self.requests_daily_amount_limit_counter
+
+        self.__requests_hourly_amount_limit = self.requests_hourly_amount_limit
+        self.__requests_hourly_amount_limit_counter = self.requests_hourly_amount_limit_counter
 
         if ref_cache_month < 1:
             ref_cache_month = 1
@@ -55,11 +64,12 @@ class VTAutomator(ABC):
         # threading lock
         self.lock1 = Lock()
         self.lock2 = Lock()
+        self.lock3 = Lock()
 
-        # cache dicts
         self.__cache_url_dict = dict()
         self.__cache_file_dict = dict()
 
+        # cache dicts
         try:
             # open and read the data from two json files 'vt_cache_url.json' and 'vt_cache_file.json'
             if not os.path.exists('vt_cache_url.json'):
@@ -83,6 +93,14 @@ class VTAutomator(ABC):
     # _____property_____ #
 
     @property
+    def api_key(self) -> str:
+        return self.__api_key
+
+    @property
+    def get_user_quota_summary(self) -> str:
+        return self.__USER_QUOTA_SUMMARY
+
+    @property
     def get_vt_api_url(self) -> str:
         return self.__GET_VT_API_URL
 
@@ -99,20 +117,36 @@ class VTAutomator(ABC):
         return self.__POST_VT_API_FILE
 
     @property
-    def requests_amount_limit(self) -> int:
-        return self.__requests_amount_limit
+    def requests_monthly_amount_limit(self) -> int:
+        return self.__requests_monthly_amount_limit
 
     @property
-    def requests_amount_limit_counter(self) -> int:
-        return self.__requests_amount_limit_counter
+    def requests_monthly_amount_limit_counter(self) -> int:
+        return self.__requests_monthly_amount_limit_counter
 
     @property
-    def requests_per_minute_limit(self) -> int:
-        return self.__requests_per_minute_limit
+    def requests_daily_amount_limit(self) -> int:
+        return self.__requests_daily_amount_limit
 
     @property
-    def requests_per_minute_limit_counter(self) -> int:
-        return self.__requests_per_minute_limit_counter
+    def requests_daily_amount_limit_counter(self) -> int:
+        return self.__requests_daily_amount_limit_counter
+
+    @property
+    def requests_hourly_amount_limit(self) -> int:
+        return self.__requests_hourly_amount_limit
+
+    @property
+    def requests_hourly_amount_limit_counter(self) -> int:
+        return self.__requests_hourly_amount_limit_counter
+
+    # @property
+    # def requests_per_minute_limit(self) -> int:
+    #     return self.__requests_per_minute_limit
+
+    # @property
+    # def requests_per_minute_limit_counter(self) -> int:
+    #     return self.__requests_per_minute_limit_counter
 
     @property
     def ref_cache_month(self) -> int:
@@ -147,34 +181,48 @@ class VTAutomator(ABC):
         pass
 
     # _____setters_____#
+    # monthly limit
+    def set_requests_monthly_amount_limit(self, limit: int) -> None:
+        self.__requests_monthly_amount_limit = limit
 
-    def set_amount_limit_counter(self) -> None:
+    # monthly limit counter
+    def set_requests_monthly_amount_limit_counter(self, limit: int) -> None:
+        self.__requests_monthly_amount_limit_counter = limit
+
+    # daily limit
+    def set_requests_daily_amount_limit(self, limit: int) -> None:
+        self.__requests_daily_amount_limit = limit
+
+    # daily limit counter
+    def set_requests_daily_amount_limit_counter(self, limit: int) -> None:
+        self.__requests_daily_amount_limit_counter = limit
+
+    # hourly limit
+    def set_requests_hourly_amount_limit(self, limit: int) -> None:
+        self.__requests_hourly_amount_limit = limit
+
+    # hourly limit counter
+    def set_requests_hourly_amount_limit_counter(self, limit: int) -> None:
+        self.__requests_hourly_amount_limit_counter = limit
+
+    def set_api_key(self, key: str = None) -> None:
+        self.__api_key = key
+
+    # update counters
+    def set_limit_counters(self) -> None:
         with self.lock1:
-            self.__requests_amount_limit_counter += 1.
-
-    def set_per_minute_limit_counter(self) -> None:
-        with self.lock2:
-            self.__requests_amount_limit_counter += 1
-
-    def set_amount_limit_refresh(self) -> None:
-        day = timedelta(hours=24)
-        while True:
-            time.sleep(day.total_seconds())
-            self.__requests_amount_limit_counter = 0
-
-    def set_per_minute_limit_refresh(self) -> None:
-        while True:
-            time.sleep(60)
-            self.__requests_amount_limit_counter = 0
+            self.__requests_monthly_amount_limit_counter += 1
+            self.__requests_daily_amount_limit_counter += 1
+            self.__requests_hourly_amount_limit_counter += 1
 
     # update dict from url decorator
-    def _update_cache_url_dict(self, url_inx, result):
-        with self.lock1:
+    def _update_cache_url_dict(self, url_inx, result) -> None:
+        with self.lock2:
             self.__cache_url_dict[url_inx] = result
 
     # update dict from file decorator
-    def _update_cache_file_dict(self, path, result):
-        with self.lock2:
+    def _update_cache_file_dict(self, path, result) -> None:
+        with self.lock3:
             self.__cache_file_dict[path] = result
 
     # ______decorators______ #
@@ -191,6 +239,7 @@ class VTAutomator(ABC):
         :param func:
         :return:
         """
+
         @functools.wraps(func)
         def wrapper(*args):
             url = args[1]
@@ -225,6 +274,7 @@ class VTAutomator(ABC):
         :param func:
         :return:
         """
+
         @functools.wraps(func)
         def wrapper(*args):
             path = args[1]
@@ -256,3 +306,46 @@ class VTAutomator(ABC):
 
         with open('vt_cache_file.json', 'w') as fh2:
             json.dump(self.cache_file_dict, fh2)
+
+    def _get_user_quora_summary(self) -> dict[str, dict]:
+
+        headers: dict = {
+            "accept": "application/json",
+            "x-apikey": self.api_key
+        }
+        # API request
+        req: 'requests' = requests.get(url=self.get_user_quota_summary + self.api_key + '/overall_quotas',
+                                       headers=headers)
+        if req.status_code >= 400:
+            raise vt_exeptions.RequestFailed()
+        elif bool(req.json()):
+            # return dict[str, dict]
+            return req.json()
+        else:
+            raise vt_exeptions.EmptyContentError()
+
+    def _get_allaowens(self):
+        summary: dict = self._get_user_quora_summary()
+        if summary is not None:
+            hourly_info = summary.get("data")["api_requests_hourly"]
+            self.set_requests_hourly_amount_limit(hourly_info["user"]["allowed"])
+            self.set_requests_hourly_amount_limit_counter(hourly_info["user"]["used"])
+
+            daily_info = summary.get("data")["api_requests_daily"]
+            self.set_requests_daily_amount_limit(daily_info["user"]["allowed"])
+            self.set_requests_daily_amount_limit_counter(daily_info["user"]["used"])
+
+            monthly_info = summary.get("data")["api_requests_monthly"]
+            self.set_requests_monthly_amount_limit(monthly_info["user"]["allowed"])
+            self.set_requests_monthly_amount_limit_counter(monthly_info["user"]["used"])
+
+    def _restrictions(self) -> bool:
+        if self.requests_hourly_amount_limit_counter < self.requests_hourly_amount_limit \
+                or self.requests_daily_amount_limit_counter < self.requests_daily_amount_limit \
+                or self.requests_monthly_amount_limit_counter < self.requests_monthly_amount_limit:
+            return True
+        else:
+            return False
+
+
+
