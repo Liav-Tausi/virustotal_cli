dd341c8c2238c556c36012811fc8cf71e1feae00
liav tausi <liavt242@gmail.com>
2023-01-13 18:55:10 +0200

    main

--- virustotal_automator/vt_main.py
+++ virustotal_automator/vt_main.py
@@ -4,12 +4,18 @@ created by: liav tausi
 date: 1/12/2023
 """
 
-
 import vt_terminal
+import time
+
 
 
 if __name__ == '__main__':
+    start = time.perf_counter()
     print(vt_terminal.main())
+    end = time.perf_counter()
+    print(round(end - start, 2), 'second(s)')
+
+
 
 
 
